33c8aa5fb9130cb512d37ed83f3f2669d45ea8f3
lucas874 <lsc500@outlook.dk>
2023-06-10 09:54:30 +0200

    split

--- Plot.fs
+++ Plot.fs
@@ -5,13 +5,20 @@ open Plotly.NET
 open Plotly.NET.LayoutObjects // this namespace contains all object abstractions for layout styling
 
 type Plot = P
-let absTree (Node ((label, x), subTrees)) =
-    let rec aux (prevX: float) (y: float) (Node ((label, x), subTrees)) =
+
+let absTree (Node((label, x), subTrees)) =
+    let rec aux (prevX: float) (y: float) (Node((label, x), subTrees)) =
         Node((label, ((prevX + x), (y))), List.map (aux (prevX + x) (y - 1.0)) subTrees) in
 
     aux 0 0 (Node((label, x), subTrees))
 
-let rec treeToPoints (Node ((label : 'a, (x: float, y: float)), subTrees)) =
+let splitString n s =
+    s
+    |> List.ofSeq
+    |> List.splitInto (String.length s / n)
+    |> List.map ((fun l -> l |> List.fold (fun a b -> string a + string b)) "")
+
+let rec treeToPoints (Node((label: 'a, (x: float, y: float)), subTrees)) =
     Chart.Point(
         [ (x, y) ],
         MultiText = [ string label ],
@@ -21,25 +28,25 @@ let rec treeToPoints (Node ((label : 'a, (x: float, y: float)), subTrees)) =
     )
     :: List.collect treeToPoints subTrees
 
-let rec verticalLines (Node ((_, ((x: float), (y: float))), subTrees)) =
+let rec verticalLines (Node((_, ((x: float), (y: float))), subTrees)) =
     match subTrees with
     | [] -> []
     | _ ->
-        Chart.Line([ x; x ], [ y-0.125; y - 0.5 ], LineColor = Color.fromString "black", ShowLegend = false)
+        Chart.Line([ x; x ], [ y - 0.125; y - 0.5 ], LineColor = Color.fromString "black", ShowLegend = false)
         :: List.map
-            (fun (Node ((_, (x, y)), _)) ->
-                Chart.Line([ x; x ], [ y+0.15; y + 0.5 ], LineColor = Color.fromString "black", ShowLegend = false))
+            (fun (Node((_, (x, y)), _)) ->
+                Chart.Line([ x; x ], [ y + 0.15; y + 0.5 ], LineColor = Color.fromString "black", ShowLegend = false))
             subTrees
         @ List.collect verticalLines subTrees
 
-let getCoord (Node ((_, ((x: float), (y: float))), _)) = x, y
+let getCoord (Node((_, ((x: float), (y: float))), _)) = x, y
 
 let rec getCoords (subTrees: Tree<'a * (float * float)> list) =
     match subTrees with
     | [] -> []
-    | Node ((_, (x, y)), _) :: rest -> (x, y) :: getCoords rest
+    | Node((_, (x, y)), _) :: rest -> (x, y) :: getCoords rest
 
-let rec horizontalLines (Node ((_, (_, _)), subTrees)) =
+let rec horizontalLines (Node((_, (_, _)), subTrees)) =
     match subTrees |> getCoords with
     | [] -> []
     | (_, _) :: [] -> List.collect horizontalLines subTrees
@@ -55,10 +62,5 @@ let rec horizontalLines (Node ((_, (_, _)), subTrees)) =
 let pointsAndLines (t: Tree<'a * (float * float)>) =
     treeToPoints t @ verticalLines t @ horizontalLines t
 
-let plot t = 
-    t
-    |> design
-    |> absTree
-    |> pointsAndLines
-    |> Chart.combine
-    |> Chart.show
+let plot t =
+    t |> design |> absTree |> pointsAndLines |> Chart.combine |> Chart.show
