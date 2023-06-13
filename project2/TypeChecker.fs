ee82d92ceab697ecbd0bb216263d1c2934bc50f7
lucas874 <lsc500@outlook.dk>
2023-06-13 20:25:06 +0200

    dasdas

--- project2/TypeChecker.fs
+++ project2/TypeChecker.fs
@@ -4,24 +4,22 @@ open FParsec
 open AST
 open Parser 
 
-
 let isDisjoint (s1: Set<'a>, s2: Set<'a>) = (s1, s2) ||> Set.intersect = Set.empty
 
+let getRWArith a = 
+    match a with 
+    | Ld(src, dst) -> Set.empty |> Set.add src, Set.empty |> Set.add dst
+    | Add(src1, src2, dst) -> (Set.empty |> Set.add src1 |> Set.add src2), Set.empty |> Set.add dst
+    | Sub(src1, src2, dst) -> (Set.empty |> Set.add src1 |> Set.add src2), Set.empty |> Set.add dst 
+    | Mul(src1, src2, dst) -> (Set.empty |> Set.add src1 |> Set.add src2), Set.empty |> Set.add dst 
+    | Div(src1, src2, dst) -> (Set.empty |> Set.add src1 |> Set.add src2), Set.empty |> Set.add dst 
+    | Sqrt(src, dst) -> Set.empty |> Set.add src, Set.empty |> Set.add dst
 
-(* let checkC c = 
-    match c with 
-    | Mdl(m) -> checkModule m
-    | Cond(con) -> checkCond con 
-
- *)
-
-
-let getRWArith a = failwith "alala"
 
 let getRWModule m = 
     match m with 
     | Ar(a) -> getRWArith a
-    | Comp(_) -> Set.empty<'a>, Set.empty<'a>
+    | Comp(_) -> Set.empty<'a>, Set.empty<'a> // what do comp
 
 let getRWCond con = failwith "not implemented"
 let getRWC c =
@@ -38,7 +36,6 @@ let rec getRWCL cl =
         let (r2, w2) = getRWCL cl2 
         (Set.union r1 r2, Set.union w1 w2)
 
-
 let rec checkRoot r =
     match r with 
     | Conc(_, _) -> true 
@@ -49,20 +46,8 @@ let rec checkRs rs : bool =  //(Set<'a> * Set<'a>) list = failwith "lala"
     | R(r) -> checkRoot r
     | RL(r1, r2) -> checkRs r1 && checkRs r2
     
-    
-
-
 let checkReadWrite (Crn(rs)) =
     rs |> checkRs 
-
-(* 
-    let rec aux (Crn(rs)) (srcs: Set<string>) (dsts: Set<String>) : bool =
-        match rs with  
-        | Root(r) -> checkRoot(r)
-        | RootList(r1, r2) ->  checkRoot(r1) && c
- *)
-
-
 
 let analysis (input: string) = 
     match parseString input with 
