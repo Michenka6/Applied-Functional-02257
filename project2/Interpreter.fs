528c072c8ed4f0acd3509e4e4e7c3e3dd1a1e243
lucas874 <lsc500@outlook.dk>
2023-06-14 10:59:49 +0200

    workings

--- project2/Interpreter.fs
+++ project2/Interpreter.fs
@@ -21,30 +21,56 @@ type Status =
       floatVariables: Map<string, float> } *)
 
 type Concentrations = Map<string, float>
+type Flags = Map<string, bool> // is continuous somehwo but..
 
 type State =
     { status: Status
       concentrations: Concentrations
+      flags: Flags
       nSteps: int }
 
-let intepretStep (stp: Step) (state: State) = failwith "not implemented"
+let comparison com state = failwith "not implemented"
 
-let rec intepretSteps (steps: Step list) (state: State) = 
+let arithmetic a state = 
+    failwith "not implemented"
+
+let interpretConditional con state = failwith "not implemented"
+
+let intepretModule m state =
+    match m with 
+    | Ar(a) -> arithmetic a state 
+    | Comp(com) -> comparison com state 
+
+let interpretCommand (cmd: Command) (state: State) =
+    let cncs = state.concentrations 
+    match cmd with 
+    | Mdl(m) -> intepretModule m state 
+    | Cond(con) -> interpretConditional con state
+
+let rec interpretStep (cmds: CommandList) (state: State) =
+    match cmds with 
+    | C(c) -> interpretCommand c state 
+    | CL(cl1, cl2) -> interpretStep cl1 state |> (interpretStep cl2)
+
+let rec interpretSteps (steps: Step list) (state: State) = 
     match steps with 
     | [] -> state 
-    | stp::steps -> intepretSteps steps (intepretStep stp state)
+    | (St(c))::steps -> interpretSteps steps (interpretStep c state)
 
 let rec stateSequence steps state n =
     seq {
       match n with
-        | 0 -> yield intepretSteps steps state
+        | 0 -> yield interpretSteps steps state
         | n when n > 0 -> 
                yield state 
                yield! stateSequence steps state (n-1)
         | _ -> failwith "negative number"
     }
 
-let initializeConcs (concs: Conc list) =
+let initFlags = 
+    Map.empty |> Map.add "Xgty" false |> Map.add "Xlty" false |> Map.add "Ygtx" false |> Map.add "Xlty" false
+
+let initConcs (concs: Conc list) =
     let rec loop c =
         function
         | [] -> c Map.empty
@@ -66,11 +92,13 @@ let rec splitRoots (rs: RootList) : Conc list * Step list =
 
 let interpret (Crn(rs)) (nSteps: int) =
     let concs, steps = splitRoots rs
-    let initCncs = initializeConcs concs
+    let initCncs = initConcs concs
+    let initFlgs = initFlags 
 
     let state0 =
         { status = Running
           concentrations = initCncs
+          flags = Map.empty<string, bool>
           nSteps = nSteps }
 
     (stateSequence steps state0 nSteps) |> Seq.append (seq { state0 })
--- project2/Program.fs
+++ project2/Program.fs
@@ -56,4 +56,4 @@ let fac =  "crn={
 
 tryParse fac    
 
-//analysis p1 5 |> printfn "%A" 
+analysis fac 5 |> printfn "%A" 
